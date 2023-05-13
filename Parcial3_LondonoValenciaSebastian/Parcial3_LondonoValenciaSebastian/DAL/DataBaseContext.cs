db7e8d0479192c87da8b4f81f414857de214a5dd
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-05-13 16:43:46 -0500

    Create New Project and DataBase with the Entity Services

---
+++ Parcial3_LondonoValenciaSebastian/Parcial3_LondonoValenciaSebastian/DAL/DataBaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial3_LondonoValenciaSebastian.DAL.Entities;
+using System.Diagnostics.Metrics;
+
+namespace Parcial3_LondonoValenciaSebastian.DAL
+{
+    public class DataBaseContext :DbContext
+    {
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+        // Se mapea la identidad para convertirla en una tabla
+        public DbSet<Service> Services { get; set; }
+
+        //Vamos a crear un índice para la tabla Countries
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Service>().HasIndex(c => c.Name).IsUnique();
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Parcial3_LondonoValenciaSebastian/Parcial3_LondonoValenciaSebastian/DAL/Entities/Service.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Parcial3_LondonoValenciaSebastian.DAL.Entities
+{
+    public class Service
+    {
+
+        [Key]
+        public virtual Guid Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal Price { get; set; }
+    }
+}
