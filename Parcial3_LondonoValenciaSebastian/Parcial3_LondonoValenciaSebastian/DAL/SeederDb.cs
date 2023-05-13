7617241131fbf0891a70c8e7987af8be26d40524
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-05-13 17:17:24 -0500

    Create Method PopulateServicesAsync

--- Parcial3_LondonoValenciaSebastian/Parcial3_LondonoValenciaSebastian/DAL/SeederDb.cs
+++ Parcial3_LondonoValenciaSebastian/Parcial3_LondonoValenciaSebastian/DAL/SeederDb.cs
@@ -1,4 +1,6 @@
-﻿namespace Parcial3_LondonoValenciaSebastian.DAL
+﻿using Parcial3_LondonoValenciaSebastian.DAL.Entities;
+
+namespace Parcial3_LondonoValenciaSebastian.DAL
 {
     public class SeederDb
     {
@@ -7,6 +9,26 @@
         public SeederDb(DataBaseContext context)
         {
             _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+        }
+
+        //Metodo para poblar la base de datos 
+        private async Task PopulateServicesAsync()
+        {
+            if (!_context.Services.Any())
+            {
+                _context.Services.Add(new Service { Id = Guid.NewGuid(), Name = "Lavada simple", Price = 25000 });
+                _context.Services.Add(new Service { Id = Guid.NewGuid(), Name = "Lavada + Polishada", Price = 50000 });
+                _context.Services.Add(new Service { Id = Guid.NewGuid(), Name = "Lavada + Aspirada de Cojinería", Price = 30000 });
+                _context.Services.Add(new Service { Id = Guid.NewGuid(), Name = "Lavada Full", Price = 65000 });
+                _context.Services.Add(new Service { Id = Guid.NewGuid(), Name = "Lavada en seco del Motor", Price = 80000 });
+                _context.Services.Add(new Service { Id = Guid.NewGuid(), Name = "Lavada Chasis", Price = 90000 });
+            }
         }
     }
 }
